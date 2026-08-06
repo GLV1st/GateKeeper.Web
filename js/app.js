6fbdc2309171054ca1833edcd54ef01f28052ff1
GLV1ST <GLV@1strichmond.org.uk>
2026-08-06 13:23:26 +0100

    Initial GateKeeper application structure

---
+++ js/app.js
@@ -0,0 +1,25 @@
+console.log("GateKeeper Started");
+
+document.getElementById("btnCheckIn").onclick=()=>{
+
+    alert("Check In screen coming next.");
+
+};
+
+document.getElementById("btnCheckOut").onclick=()=>{
+
+    alert("Check Out screen coming next.");
+
+};
+
+document.getElementById("btnStatus").onclick=()=>{
+
+    alert("Status screen coming next.");
+
+};
+
+document.getElementById("btnSettings").onclick=()=>{
+
+    alert("Settings screen coming next.");
+
+};
